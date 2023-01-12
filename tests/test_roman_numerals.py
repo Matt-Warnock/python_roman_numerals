e76926d71b70f3faf49b06f138d18146bd80fc17
MattW <mattnwarnock@yahoo.co.uk>
2023-01-12 11:04:23 +0000

    Pass convert 3

--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -6,7 +6,10 @@ class TestRomanNumerals:
         number = 1
         assert RomanNumerals().convert(number) == "I"
 
-
     def test_converts_two_to_ii(self):
         number = 2
         assert RomanNumerals().convert(number) == "II"
+
+    def test_converts_two_to_iii(self):
+        number = 3
+        assert RomanNumerals().convert(number) == "III"
