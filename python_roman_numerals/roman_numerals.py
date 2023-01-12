17272b105b2c18ed1102a8b50083fa2e05317fc5
MattW <mattnwarnock@yahoo.co.uk>
2023-01-12 11:06:19 +0000

    Refactor

--- python_roman_numerals/roman_numerals.py
+++ python_roman_numerals/roman_numerals.py
@@ -1,8 +1,3 @@
 class RomanNumerals:
     def convert(self, number: int) -> str:
-        if number == 1:
-            return 'I'
-        if number == 2:
-            return 'II'
-        if number == 3:
-            return 'III'
+        return 'I' * number
