6c1938e61f0b4b80b465d835791b9ab080d07127
Tim Turvey <tturvey@hotmail.co.uk>
2023-01-20 14:55:50 +0000

    Converts 10 to X

--- python_roman_numerals/roman_numerals.py
+++ python_roman_numerals/roman_numerals.py
@@ -2,6 +2,10 @@ class RomanNumerals:
     def convert(self, number: int) -> str:
         numeral = ''
 
+        if number >= 10:
+            numeral = 'X'
+            number -= 10
+
         if number >= 5:
             numeral = 'V'
             number -= 5
--- tests/test_roman_numerals.py
+++ tests/test_roman_numerals.py
@@ -8,7 +8,8 @@ class TestRomanNumerals:
         (3, 'III'),
         (5, 'V'),
         (6, 'VI'),
-        (7, 'VII')
+        (7, 'VII'),
+        (10, 'X')
     ]
 
     test_names = [
@@ -17,9 +18,11 @@ class TestRomanNumerals:
         'Converts 3 to III',
         'Converts 5 to V',
         'Converts 6 to VI',
-        'Converts 7 to VII'
+        'Converts 7 to VII',
+        'Converts 10 to X'
     ]
 
     @pytest.mark.parametrize('number, numeral', tests, ids=test_names)
     def test_all_numbers_to_numerals(self, number, numeral, roman_numeral):
         assert roman_numeral.convert(number) == numeral
+
